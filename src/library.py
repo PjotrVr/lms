952aad16d2ece3d124f534e02ed6d550fa203748
PjotrVr <stika.petar@gmail.com>
2023-07-07 00:18:40 +0200

    add return_book function

--- src/library.py
+++ src/library.py
@@ -1,5 +1,6 @@
 import sqlite3
 from enum import Enum
+from datetime import date, datetime
 
 
 class BorrowResult(Enum):
@@ -7,6 +8,12 @@ class BorrowResult(Enum):
     BOOK_AVAILABLE = 2
     BOOK_UNAVAILABLE = 3
 
+
+class ReturnResult(Enum):
+    NOT_BORROWED = 1
+    RETURN_SUCCESSFUL = 2
+    RETURN_OVERDUE = 3
+    
 
 def borrow_book(cursor, jmbag, book_id, borrow_date, return_date):
     # check if the book exists
@@ -27,3 +34,26 @@ def borrow_book(cursor, jmbag, book_id, borrow_date, return_date):
     cursor.execute("INSERT INTO borrowed_books (jmbag, book_id, borrow_date, return_date) VALUES(?, ?, ?, ?)", (jmbag, book_id, borrow_date, return_date))
 
     return BorrowResult.BOOK_AVAILABLE
+
+
+def return_book(cursor, jmbag, book_id, current_date):
+    # check if the book is currently borrowed by the student
+    cursor.execute("SELECT return_date FROM borrowed_books WHERE jmbag = ? AND book_id = ?", (jmbag, book_id))
+    result = cursor.fetchone()
+
+    if result is None:
+        return ReturnResult.NOT_BORROWED
+
+    return_date = datetime.strptime(result[0], '%Y-%m-%d').date()
+
+    # increase the available copies of the book
+    cursor.execute("UPDATE books SET available_copies = available_copies + 1 WHERE book_id = ?", (book_id,))
+
+    # remove the entry from the borrowed_books table
+    cursor.execute("DELETE FROM borrowed_books WHERE jmbag = ? AND book_id = ?", (jmbag, book_id))
+
+    if return_date > current_date:
+        return ReturnResult.RETURN_OVERDUE
+    
+    else:
+        return ReturnResult.RETURN_SUCCESSFUL
